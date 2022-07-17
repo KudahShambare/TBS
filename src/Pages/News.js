f2d5917c0b11a02b43243ae80047992c0bf86b0a
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-07-17 22:02:57 +0200

    News Page Done

--- src/Pages/News.js
+++ src/Pages/News.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../Components/Footer";
 import OtherNavBar from "../Components/OtherNavBar";
 
@@ -6,7 +7,28 @@ const News = () => {
   return (
     <div className="pages">
       <OtherNavBar page="News" />
-      <div id="news"></div>
+      <div id="news">
+        <section>
+          <h1>
+            Stay up to date with Tando Business Solutions news and technological
+            ventures{" "}
+          </h1>
+          <hr />
+          <img
+            id="news-img"
+            alt="Ooops"
+            src="https://www.tandobs.co.za/wp-content/uploads/2021/03/digital-transformation-article-900x313.png"
+          />
+          <div>
+            {" "}
+            <h1>
+              {" "}
+              Digital transformation's impact on businesses within South Africa
+            </h1>{" "}
+            <Link to=""> Read More >>> </Link>
+          </div>
+        </section>
+      </div>
       <Footer />
     </div>
   );
