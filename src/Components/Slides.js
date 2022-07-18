49678a0c571c17abe9b379b6a071e384516c9975
Kudakwashe Shambare <70160442+KudahShambare@users.noreply.github.com>
2022-07-18 15:14:13 +0200

    Title updated

--- src/Components/Slides.js
+++ src/Components/Slides.js
@@ -1,6 +1,21 @@
 import React from "react";
 
 const Slides = () => {
-  return <div>slides</div>;
+  return (
+    <div>
+      <h1
+        style={{
+          position: "absolute",
+          top: "20vh",
+          left: "20vw",
+          color: "orangered",
+          backgroundColor: "white"
+        }}
+      >
+        {" "}
+        Notice: Real Images From The Photographer To Replace The Current Ones
+      </h1>
+    </div>
+  );
 };
 export default Slides;
